c33efa26e4bfd68af7b2311e9043b6380c76665a
braianferreira <105357820+braianferreira@users.noreply.github.com>
2024-10-11 08:35:50 -0300

    ok

---
+++ graficos/informaçoesGlobais.js
@@ -0,0 +1,5 @@
+const url = 'https ; //raw. githubusercontent.com/guilhermeonsrails/api/main/dados-globais.json'
+async function  visualisarininformacoesglobais(0){
+    const res = await fetch(url)
+    console.log(dados);
+} 
