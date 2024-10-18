3c13f4eb805883e97009ddd3b1054fb2d46e490c
braianferreira <105357820+braianferreira@users.noreply.github.com>
2024-10-18 08:29:45 -0300

    ok

--- graficos/informaçoesGlobais.js
+++ graficos/informaçoesGlobais.js
@@ -1,5 +1,16 @@
-const url = 'https ; //raw. githubusercontent.com/guilhermeonsrails/api/main/dados-globais.json'
-async function  visualisarininformacoesglobais(0){
+const url = 'https://raw.githubusercontent.com/guilhermeonsrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
-    console.log(dados);
-} 
+    const dados = await res.json()
+    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
+  
